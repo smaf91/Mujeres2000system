3d8f4f1d45de6df8f75af7ea5618a151d28caf06
Dalal Missri <56326528+dalal1690@users.noreply.github.com>
2020-12-09 10:30:59 -0300

    estructura
    Creacion de la clase usuario con JPA. (verificar javax validations)

---
+++ system/system/src/main/java/com/mujeres2000/system/model/Usuario.java
@@ -0,0 +1,71 @@
+package com.mujeres2000.system.model;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.Min;
+
+@Entity
+@Table(name = "usuario")
+//Creacion de tabla de usuarios.
+
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int usuario_ID;
+
+    //   @Email(message = "Ingrese su dirección de correo electrónico") //valida que sea un mail
+    private String email;
+
+    //   @NotNull (message = "Inserte su contraseña")
+    private String password;
+
+    @OneToMany (mappedBy="usuario", cascade = CascadeType.ALL, fetch = FetchType.LAZY )
+    private List<producto> productos = new ArrayList<>();
+
+//    private List<producto> ListaProducto;
+
+    //CONSTRUCTOR
+
+    public Usuario(int usuario_ID, String email, String password) {
+        this.usuario_ID = usuario_ID;
+        this.email = email;
+        this.password = password;
+    }
+
+    //GETTERS AND SETTERS
+
+    public int getUsuario_ID() {
+        return usuario_ID;
+    }
+
+    public void setUsuario_ID(int usuario_ID) {
+        this.usuario_ID = usuario_ID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<producto> productos) {
+        this.productos = productos;
+    }
+}
+
---
+++ system/system/src/test/java/com/mujeres2000/system/Mujeres2000SystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mujeres2000.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mujeres2000SystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
