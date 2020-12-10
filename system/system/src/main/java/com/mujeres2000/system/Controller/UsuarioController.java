70a75763eaef09dece218ce6bdefed35c6fcd0fc
Dalal Missri <56326528+dalal1690@users.noreply.github.com>
2020-12-10 08:52:27 -0300

    Usuario Controller con cambios

--- system/system/src/main/java/com/mujeres2000/system/Controller/UsuarioController.java
+++ system/system/src/main/java/com/mujeres2000/system/Controller/UsuarioController.java
@@ -8,6 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.ArrayList;
 //import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
@@ -19,20 +23,40 @@ public class UsuarioController {
 
     Log log = LogFactory.getLog(UsuarioController.class);
 
-    @PostMapping(path="/nuevo-usuario")
+    @PostMapping(path = "/nuevo-usuario")
     //@Operation(summary = "Guarda nuevo usuario", description= "Guarda un nuevo usuario a la base de datos")
-    public ResponseEntity<Usuario> guardarUsuario(@RequestBody Usuario usuario){
+    public ResponseEntity<Usuario> guardarUsuario(@RequestBody Usuario usuario) {
         log.info("Sistema guarda nuevo usuario: " + usuario);
         usuarioRepository.save(usuario);
-        return new ResponseEntity<>()(usuario, HttpStatus.CREATED);
+        return new ResponseEntity<>(usuario, HttpStatus.CREATED);
     }
 
-    @PutMapping(path= "/usuario")
+    @PutMapping(path = "/usuario")
     //@Operation(summary = "Modifica usuario guardado", description= "Modifica usuario guardado en la base de datos")
-    public ResponseEntity<Usuario> actualizarUsuario(@RequestBody Usuario usuario){
+    public ResponseEntity<Usuario> actualizarUsuario(@RequestBody Usuario usuario) {
         log.info("Sistema actualiza usuario: " + usuario);
         usuarioRepository.save(usuario);
         return new ResponseEntity<>(usuario, HttpStatus.CREATED);
     }
 
+    @DeleteMapping(path = "/usuario/{id}")
+    //@Operation(summary = "Elimina usuario guardado", description= "Elimina usuario guardado en la base de datos")
+    public ResponseEntity<Usuario> borrarUsuario(@PathVariable("id") Usuario usuario) {
+        log.info("Sistema elimina usuario: " + usuario);
+        usuarioRepository.delete(usuario);
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/usuario/{id}")
+    //@Operation(summary = "Busca usuario guardado", description= "Busca usuario guardado en la base de datos")
+    public ResponseEntity<Usuario> buscarUsuario(@PathVariable("id") Integer id) {
+        log.info("Sistema busca usuario: " + id);
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+        if (Optional.empty().equals(usuario)) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        } else {
+            Usuario devuelveUsuario = usuario.get();
+            return new ResponseEntity<>(devuelveUsuario, HttpStatus.OK);
+        }
+    }
 }
